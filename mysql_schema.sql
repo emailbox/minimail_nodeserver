c2cdda111e060317a5fafd4b47eb8e8bd31db9ca
emailbox <nicholas.a.reed+emailbox@gmail.com>
2013-03-06 12:19:12 -0800

    Update mysql_schema.sql

--- mysql_schema.sql
+++ mysql_schema.sql
@@ -1,10 +1,9 @@
-
 CREATE TABLE IF NOT EXISTS `f_users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `emailbox_id` varchar(255) NOT NULL,
-  `user_token` varchar(255) NOT NULL,
+  `access_token` text NOT NULL,
   `created` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `emailbox_id` (`emailbox_id`),
-  KEY `user_token` (`user_token`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1949 ;
+  KEY `user_token` (`access_token`(255))
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2358 ;
